eb57ef719cabc5611790a40244f03dc8d34bd9ca
Vinicius <viniciuspascoal013@gmail.com>
2025-07-31 08:52:58 -0300

    gave over update

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -8,17 +8,17 @@ export default function GameOverModal({
 }) {
   return (
     <motion.div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60"
+      className="fixed inset-0 flex items-center justify-center bg-[#181818c2] "
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
-      <div className="bg-white p-8 rounded-xl shadow-xl text-center">
-        <h2 className="text-2xl font-bold mb-4">Game Over</h2>
+      <div className="bg-gradient-to-b from-gray-800 to-gray-900 p-8 rounded-xl shadow-xl text-center">
+        <h2 className="text-2xl font-bold mb-4 ">Game Over</h2>
         <button
           onClick={onRestart}
           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
         >
-          Jogar Novamente
+          Jogar
         </button>
       </div>
     </motion.div>
